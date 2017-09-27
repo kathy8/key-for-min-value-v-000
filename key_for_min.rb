1fe030522bea1af5e1c005e4628ca372bb4c79bf
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-27 18:01:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,10 @@ def key_for_min_value(name_hash)
 lowest_key = nil
 lowest_value = nil
 name_hash.each do |a, b|
-if lowest_value == nil || b < lowest_value
+if lowest_value ==nil || b < lowest_value
 lowest_value = begin
 lowest_value = a
 end
 end
 lowest_key
 end
-      
